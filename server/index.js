0b74d99238c76b72569cda392e69fa85ab1b33c2
Cody Heslep <codyheslep@gmail.com>
2018-08-01 18:30:43 -0500

    fixed

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //static middleware
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.listen(57693, () => console.log(`Listening on 57693`))
+app.listen(process.env.PORT || 5000, () => console.log(`Listening on 5000`))
 
 app.use('/api', require('./api'));
 
