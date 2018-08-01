1b211e758f5a7a186816f78da6baf5fd849e9711
Cody Heslep <codyheslep@gmail.com>
2018-08-01 18:40:38 -0500

    maybe

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.listen(process.env.PORT || 5000, () => console.log(`Listening on 5000`))
 
 app.use('/api', require('./api'));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
